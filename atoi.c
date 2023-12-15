65285d631b0bac688529cb96b3d93c7d45f0bff1
kariuki11 <irungukenneth433@gmail.com>
2023-12-15 15:15:00 -0800

    The simple shell project done on 10th December 2023

--- atoi.c
+++ atoi.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 /**
- * interactive - returns true if shell is interactive mode
+ * interactive - return true if the shell is interactive mode
  * @info: struct address
  *
  * Return: 1 if interactive mode, 0 otherwise
@@ -26,7 +26,7 @@ int is_delim(char c, char *delim)
 }
 
 /**
- *_isalpha - checks for alphabetic character
+ *_isalpha - check for alphabetic character
  *@c: The character to input
  *Return: 1 if c is alphabetic, 0 otherwise
  */
@@ -40,9 +40,9 @@ int _isalpha(int c)
 }
 
 /**
- *_atoi - converts a string to an integer
+ *_atoi - convert a string to an integer
  *@s: the string to be converted
- *Return: 0 if no numbers in string, converted number otherwise
+ *Return: 0 if no number in string, converted number otherwise
  */
 
 int _atoi(char *s)
--- built1.c
+++ built1.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
+ * _myhistory - display the history list, one command by line, precedded
+ *              with line number, starting at 0.
  * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ *        constant function prototypes.
  *  Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -14,7 +14,7 @@ int _myhistory(info_t *info)
 }
 
 /**
- * unset_alias - sets alias to string
+ * unset_alias - set alias to string
  * @info: parameter struct
  * @str: the string alias
  *
@@ -37,7 +37,7 @@ int unset_alias(info_t *info, char *str)
 }
 
 /**
- * set_alias - sets alias to string
+ * set_alias - set alias to string
  * @info: parameter struct
  * @str: the string alias
  *
@@ -58,7 +58,7 @@ int set_alias(info_t *info, char *str)
 }
 
 /**
- * print_alias - prints an alias string
+ * print_alias - print an alias string
  * @node: the alias node
  *
  * Return: Always 0 on success, 1 on error
@@ -81,7 +81,7 @@ int print_alias(list_t *node)
 }
 
 /**
- * _myalias - mimics the alias builtin (man alias)
+ * _myalias - Mimics the alias builtin (man alias)
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  *  Return: Always 0
